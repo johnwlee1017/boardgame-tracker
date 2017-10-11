214a82089312ce7a1a0539e4d97441acc82e47f4
cortney & Anson <pair+cortneynicole.hianson@devbootcamp.com>
2017-10-11 11:48:10 -0700

    create associations for user and boardgames

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -1,2 +1,3 @@
 class Boardgame < ApplicationRecord
+  belongs_to :owner, class_name: "User"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(username: 'neurodynamic', password: 'password')
-Boardgame.create(name: 'Banana Grams', description: 'make anagrams out of bananas', type: 'grammar', players: 4, user_id: User.first.id, image: 'http://bit.ly/2ybIa68')
-Boardgame.create(name: 'Scrabble', description: 'make regular words out of bananas', type: 'grammar', players: 6, user_id: User.first.id, image: 'http://bit.ly/2xzH3sC')
+Boardgame.create(name: 'Banana Grams', description: 'make anagrams out of bananas', genre: 'grammar', players: 4, owner_id: User.first.id, image: 'http://bit.ly/2ybIa68')
+Boardgame.create(name: 'Scrabble', description: 'make regular words out of bananas', genre: 'grammar', players: 6, owner_id: User.first.id, image: 'http://bit.ly/2xzH3sC')
