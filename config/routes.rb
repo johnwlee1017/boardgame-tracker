ac24012027600603973495a0e1f1f2114203880f
cortney & Anson <pair+cortneynicole.hianson@devbootcamp.com>
2017-10-11 12:26:38 -0700

    create register

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'sessions#new'
 
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create]
 end
