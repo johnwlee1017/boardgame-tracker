d80fe05e95da022ec297201cc53f1b494e004aac
cortney & Anson & John Lee & Kanji Yomoda <pair+cortneynicole.hianson.johnwlee1017.yomoda07@devbootcamp.com>
2017-10-12 12:01:31 -0700

    Create friendships table and the associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :boardgames, foreign_key: "owner_id"
+  has_many :friendships
+  has_many :friends, through: :friendships
   validates :password, :length => { :minimum => 4 }, on: :create
   validates :username, uniqueness: true, presence: true
 end
