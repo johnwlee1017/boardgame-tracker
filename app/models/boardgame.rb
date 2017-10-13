b914a0db882c6be7ec557ba138c0e7848f796c00
cortney & Anson & John Lee & Kanji Yomoda <pair+cortneynicole.hianson.johnwlee1017.yomoda07@devbootcamp.com>
2017-10-13 15:50:27 -0700

    add homelink to layout and switch logout to layout. fix picture loading bug

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -1,6 +1,6 @@
 class Boardgame < ApplicationRecord
   belongs_to :owner, class_name: "User"
- validates :name, :description, :genre, :players, :play_time, presence: true
+  validates :name, :description, :genre, :players, :play_time, presence: true
 
   def self.search(search)
     # Title is for the above case, the OP incorrectly had 'name'
@@ -13,5 +13,11 @@ class Boardgame < ApplicationRecord
     obj = s3.bucket('board-game-tracker').object(File.basename(image))
     obj.upload_file(image)
     obj.public_url
+  end
+
+  private
+
+  def check_image
+
   end
 end
